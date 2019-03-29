1b937d49134944ec5cb0ed8f09c812c55133231e
goutam814166 <43249363+goutam814166@users.noreply.github.com>
2019-03-29 08:16:24 -0700

    Create coercion.js

---
+++ coercion.js
@@ -0,0 +1,25 @@
+//console.log('5'-5);
+//const giveType=typeof {}
+//console.log(giveType);
+
+
+const adder=false+5;
+//console.log(adder);
+
+const loginDetails=''
+//logic from details from db
+const loginId=loginDetails[0]
+if(loginDetails)
+{
+	console.log('Allow user login');	
+	}
+else 
+	{
+		console.log('Auth faild');
+	}
+
+
+
+
+
+
